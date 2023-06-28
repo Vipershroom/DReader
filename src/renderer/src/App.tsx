b96216e319f6c90af5d98c8d8e626c6fb8ec152d
Vipershroom <f>
2023-06-28 01:33:04 -0400

    initial commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './styles/App.scss'
+function App(): JSX.Element {
+    return (
+        <main>
+            <h1>Hello, World!</h1>
+        </main>
+    )
+}
+
+export default App
