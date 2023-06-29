73600df248ceb0fbc1626c44b07aaf5532c2ac3e
Vipershroom <f>
2023-06-29 18:55:25 -0400

    Move data fetching to useFootGun I mean useEffect

--- src/renderer/src/components/Homepage.tsx
+++ src/renderer/src/components/Homepage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Sources from './Homepage_Components/Sources'
 import Categorys from './Homepage_Components/Categorys'
 import { MangaCard } from '../../../interfaces/interfaces'
@@ -11,22 +11,29 @@ const Homepage = () => {
             cover: '',
         },
     ])
+    useEffect(() => {
+        return () => {
+            console.log('hi')
+            window.electron.ipcRenderer.send('card-fetch')
+            window.electron.ipcRenderer.on(
+                'card-recieve',
+                (event, data: Array<MangaCard>) => {
+                    console.log(data)
+                    setCardData(data)
+                    // console.log(cardData)
+                    console.log('Done fetching')
+                }
+            )
+        }
+    }, [])
+
     return (
         <div>
             <Sources />
             <Categorys props={cardData} />
             <button
                 onClick={() => {
-                    window.electron.ipcRenderer.send('card-fetch')
-                    window.electron.ipcRenderer.on(
-                        'card-recieve',
-                        (event, data: Array<MangaCard>) => {
-                            console.log(data)
-                            setCardData(data)
-                            // console.log(cardData)
-                            console.log('Done fetching')
-                        }
-                    )
+                    console.log()
                 }}
             >
                 Hello
