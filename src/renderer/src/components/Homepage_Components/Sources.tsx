8489c65be1ef3835994db54ee9c227c0a66fe3a7
Vipershroom <f>
2023-06-28 20:25:53 -0400

    Remove an unecessary div

--- src/renderer/src/components/Homepage_Components/Sources.tsx
+++ src/renderer/src/components/Homepage_Components/Sources.tsx
@@ -3,10 +3,8 @@ import './styles/Sources.scss'
 
 const Sources = () => {
     return (
-        <div>
-            <div className="sources">
-                <button>MangaDex</button>
-            </div>
+        <div className="sources">
+            <button>MangaDex</button>
         </div>
     )
 }
