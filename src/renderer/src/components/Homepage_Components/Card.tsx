6093e25c734f23b7b7d71dba2c3d9cf03ca1bdf4
Vipershroom <f>
2023-06-29 17:44:15 -0400

    Add Card component

--- src/renderer/src/components/Homepage_Components/Card.tsx
+++ src/renderer/src/components/Homepage_Components/Card.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import './styles/Card.scss'
 const Card = () => {
-    return <div>Card</div>
+    return (
+        <>
+            <div className="card-wrapper">
+                <div className="card">
+                    <div className="description">
+                        {/*
+                        TODO
+                        Add a check for characters, character limit is: 234
+                        */}
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Vero similique nihil expedita nostrum dolore
+                            cupiditate possimus, sed fuga doloremque corporis
+                            exercitationem ut facere architecto voluptatibus.
+                            Voluptas similique perspiciatis pariatur eveniet.
+                        </p>
+                    </div>
+                </div>
+                <h2>Title</h2>
+            </div>
+        </>
+    )
 }
 
 export default Card
