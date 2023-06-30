11acb39006e8a007e5cb058bc5385463ac972a08
Vipershroom <undertalefan812@gmail.com>
2023-06-30 12:36:29 -0400

    Add navbar component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Homepage from './components/Homepage'
 
 import './styles/App.scss'
+import Navbar from './Navbar'
 function App(): JSX.Element {
     return (
         <main>
             <Homepage />
+            <Navbar />
         </main>
     )
 }
