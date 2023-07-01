a6774e81ddf9f71687a312ef82f7c71c25d5aaa1
Vipershroom <undertalefan812@gmail.com>
2023-07-01 01:08:37 -0400

    Add icons to navbar

--- src/renderer/src/Navbar.tsx
+++ src/renderer/src/Navbar.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './styles/Navbar.scss'
+import search from './assets/search.svg'
+import home from './assets/home.svg'
+import bookmark from './assets/bookmark.svg'
 const Navbar = () => {
     return (
         <nav>
             <div className="container">
-                <div>Home</div>
-                <div>Search</div>
-                <div>Bookmarks</div>
+                <div>
+                    <img src={home} alt="" />
+                </div>
+                <div className="search">
+                    <img src={search} alt="" />
+                </div>
+                <div>
+                    <img src={bookmark} alt="" />
+                </div>
             </div>
         </nav>
     )
