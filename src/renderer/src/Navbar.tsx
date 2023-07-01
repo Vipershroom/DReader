d5f72bcf5dd0e4e97f1ce6e3a0fd4a6453553ba8
Vipershroom <undertalefan812@gmail.com>
2023-07-01 00:44:14 -0400

    Style navbar component

--- src/renderer/src/Navbar.tsx
+++ src/renderer/src/Navbar.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import './styles/Navbar.scss'
 const Navbar = () => {
-    return <div>Navbar</div>
+    return (
+        <nav>
+            <div className="container">
+                <div>Home</div>
+                <div>Search</div>
+                <div>Bookmarks</div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
