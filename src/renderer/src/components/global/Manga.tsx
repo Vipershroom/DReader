c63eadd9b5385363ba42995318c94253509030d6
Vipershroom <undertalefan812@gmail.com>
2023-07-01 20:54:18 -0400

    Add tags and description divs

--- src/renderer/src/components/global/Manga.tsx
+++ src/renderer/src/components/global/Manga.tsx
@@ -6,12 +6,23 @@ const Manga = () => {
         <div>
             <div className="title-wrapper">
                 <div className="title">
-                    <img
-                        src="https://mangadex.org/covers/76ac6b53-df49-4ace-85a2-b27970830ef2/24edd0f4-234a-48a6-9bbe-fa6b667cee01.jpg"
-                        alt=""
-                    />
+                    <div className="image-container">
+                        <img
+                            src="https://mangadex.org/covers/76ac6b53-df49-4ace-85a2-b27970830ef2/24edd0f4-234a-48a6-9bbe-fa6b667cee01.jpg"
+                            alt=""
+                        />
+                        <div className="tags">
+                            <p>Tag1</p>
+                            <p>Tag2</p>
+                            <p>Tag3</p>
+                        </div>
+                    </div>
                     <h1>Title</h1>
                 </div>
+                <div className="description">
+                    <div className="content"></div>
+                </div>
+                <div className="chapters"></div>
             </div>
         </div>
     )
